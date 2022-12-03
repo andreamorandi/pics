89e3394022af77203a5d918e2e31ea1505758395
andreamorandi <morandiandrearocco@gmail.com>
2022-12-03 14:46:30 +0100

    axios implementation

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseUrl: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID w5e8YeUw35Ljg0JuTy1fD02lR0rceUia-RSTAIPxMv4'
+    }
+});
